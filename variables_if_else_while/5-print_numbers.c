253fc33eeb0e19c26ceb8aae510794d1c37e0e5f
fadumaaah <10507@holbertonstudents.com>
2025-02-24 04:11:27 +0000

    5-print-numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - 0-10  by new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 0;
+	
+	while (n  < 10)
+	{
+	printf("%d", n);
+	n = n + 1;
+	}
+	printf("\n");
+	return (0);
+}

acdc8a4c706c2541f4b368c36c6ffc511b89ad79
fadumaaah <10507@holbertonstudents.com>
2025-02-26 22:03:48 +0000

    hotfix missing inner while brackets

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -17,11 +17,13 @@ void jack_bauer(void)
 	{
 		min = 0;
 		while (min <= 59)
+		{
 		_putchar((hour / 10) + '0');
 		_putchar((hour % 10) + '0');
 		_putchar(':');
 		_putchar((min / 10) + '0');
 		_putchar((min % 10) + '0');
 		_putchar('\n');
+		}
 	}
 }

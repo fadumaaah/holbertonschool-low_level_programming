3ff30c5f89070c45854c8912073428fec5197be9
fadumaaah <10507@holbertonstudents.com>
2025-02-26 20:19:54 +0000

    test 1-aphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main -  Prints lowercase alphabet followed by new line
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <='z') {
+	_putchar(c);
+	c = c + 1;
+	}
+	_putchar('\n');
+}
+
+

6880143ddeafe6c7538babce0dd6df808e586d12
fadumaaah <10507@holbertonstudents.com>
2025-02-26 20:51:54 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha -  return 1 if c is an alphabetic character else return 0
+ *
+ * @c: param to check
+ *
+ * Return:  1 if an alphabetic character, 0 if not
+ *
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
+}

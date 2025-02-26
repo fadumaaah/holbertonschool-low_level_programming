b8c71d5197327cd8f3ddcd73a9436af2ea97e752
fadumaaah <10507@holbertonstudents.com>
2025-02-26 22:00:41 +0000

    8-24.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+* jack_bauer - print every minute from 00:00 to 23:59
+*
+* Return: Always 0
+*
+*/
+
+void jack_bauer(void)
+{
+	int hour = 0;
+	int min = 0;
+
+	while (hour <= 23)
+	{
+		min = 0;
+		while (min <= 59)
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+	}
+}

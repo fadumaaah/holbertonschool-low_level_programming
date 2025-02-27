e36fe535f501b20d05311da6fa656393cbbf25c4
fadumaaah <10507@holbertonstudents.com>
2025-02-27 23:17:44 +0000

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Print numbers 0 - 14 followed by new line 10 times.
+ *
+ * Return : void
+ *
+ */
+
+void more_numbers(void)
+{
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		int num = 0;
+
+		while (num < 15)
+		{
+		_putchar(num + '0');
+		num = num + 1;
+		}
+		_putchar('\n');
+		i = i + 1;
+	}
+
+}

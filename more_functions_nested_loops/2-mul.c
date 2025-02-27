0af3137a3050ae74045da796c9eb62815ec506ba
fadumaaah <10507@holbertonstudents.com>
2025-02-27 22:24:43 +0000

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Multiply two integers print result
+ *
+ * @a: integer 1 to multiply
+ * @b: integer 2 to multiply
+ *
+ *  Return: Always 0
+ *
+ */
+
+int mul(int a, int b)
+{
+	int n = a * b;
+
+	printf("%d", n);
+	return (0);
+}

5bb4b12766e074da383390e17ef25a5059690a52
fadumaaah <10507@holbertonstudents.com>
2025-02-27 01:14:43 +0000

    updated format

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,28 +10,21 @@
 
 void print_to_98(int n)
 {
-	while (n > 98)
+	if (n < 98)
 	{
-		if (n == 98)
+		while (n < 98)
 		{
 			printf("%d\n", n);
+			n = n + 1;
 		}
-		else
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n = n - 1;
 		}
 	}
-	while (n <=  98)
-	{
-		if (n < 98)
-		{
-			printf("%d, ", n);
-			n = n + 1;
-		}
-		else
-		{
-			printf("%d\n", n);
-		}
-	}
+	printf("%d\n", n);
 }

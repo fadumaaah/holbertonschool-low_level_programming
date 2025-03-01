cb3763efc5621ead18c0515c511ed6b64de30860
fadumaaah <10507@holbertonstudents.com>
2025-03-01 14:00:17 +0000

    7-print_diagnal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - Draw a diagonal line by printing  \ n times
+ *
+ *@n: number of times _ should be printed
+ *
+ * Return : void
+ *
+ */
+
+
+void print_diagonal(int n)
+{
+	int vert = 0;
+	int horz = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (vert < n)
+		{
+			while (horz <= vert)
+			{
+				_putchar(' ');
+				horz = horz + 1;
+			}
+			horz = 0;
+			_putchar('\\');
+			_putchar('\n);
+			vert = vert + 1;
+		}
+	}
+}

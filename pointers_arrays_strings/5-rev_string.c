9de25691f8aa050544ca32e47397793c71a770f5
fadumaaah <10507@holbertonstudents.com>
2025-03-03 03:11:13 +0000

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -11,17 +11,19 @@
 void rev_string(char *s)
 {
 	int length = 0;
+	int i;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	for (int i = 0; i < length / 2; i++)
+	for (i = 0; i < length / 2; i++)
 	{
-		s[i] = s[i] + s[length - i - 1]; // Add first and last value to gether
-		s[length - i - 1] = s[i] - s[length - i - 1]; // minuses value to get original value
-		s[i] = s[i] - s[length - i - 1]; // swaps values
+		s[i] = s[i] + s[length - i - 1]; /* Add first and last value to gether */
+		s[length - i - 1] = s[i] - s[length - i - 1];
+		/* minuses value to get original value */
+		s[i] = s[i] - s[length - i - 1]; /* swaps values */
 	}
 
 }

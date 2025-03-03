fb9e6bee5f00a91fea61582d9221e0e75bcf7c61
fadumaaah <10507@holbertonstudents.com>
2025-03-03 06:20:13 +0000

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - Converts string into integer
+ *
+ * @s: String to be converted
+ *
+ * Return: Integer value of string otherwise return 0
+ *
+ * Description: Function converts a string into an integer
+ * ignore any non-number characters, take into account any
+ * leading + or - before the number.
+ *
+ */
+
+int _atoi(char *s)
+{
+	int index = 0;
+	int result = 0;
+
+	while (s[index] == ' ') /* Skip spaces*/
+		index++;
+
+	if (s[index] == '-') /* Account for - or + */
+	{
+		result = result * -1;
+		index++;
+	}
+	else
+		index++;
+
+	while (s[index] >= '0' && s[index] <= 9) /* Account for numbers*/
+	{
+		result = result * 10 + (s[index] - '0');
+		index++;
+	}
+
+	if (result == 0)
+		return (0);
+	else
+		return (result);
+
+
+
+
+}

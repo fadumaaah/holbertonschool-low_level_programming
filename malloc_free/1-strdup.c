478386a9aa5b5de24c2ed0612d6443cc30f05b35
fadumaaah <10507@holbertonstudents.com>
2025-03-11 04:02:02 +0000

    hotfix 1-strdup forgot double **

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-/*
- *_strdup -  returns a pointer to a newly allocated space in memory
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory
  *
  * @str: string to copy
  *

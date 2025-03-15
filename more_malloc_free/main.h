2d030f34b84b7919706771a208cd2dca99bcbc41
fadumaaah <10507@holbertonstudents.com>
2025-03-15 18:00:03 +0000

    add prototypes

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,17 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
-void reset_to_98(int *n);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-void print_chessboard(char (*a)[8]);
-char *_strdup(char *str);
-
-
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
 
 #endif

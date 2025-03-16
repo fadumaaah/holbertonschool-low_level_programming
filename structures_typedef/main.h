36843cd48c96da92e4b66a01165ead350345bf1d
fadumaaah <10507@holbertonstudents.com>
2025-03-16 11:05:00 +0000

    define dog_t once

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-typedef struct dog dog_t;
-
-
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

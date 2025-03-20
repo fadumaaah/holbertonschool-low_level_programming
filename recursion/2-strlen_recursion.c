6bf4cfb5331441ade1323cac988aae300b7f7357
fadumaaah <10507@holbertonstudents.com>
2025-03-20 00:04:35 +0000

    2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string to measure
+ *
+ * Return: Length of string
+ */
+
+
+int _strlen_recursion(char *s)
+{
+	int length  = 0
+
+	if (*s)
+	{
+		length++;
+		_strlen_recursion(s + 1);
+	}
+
+	return (length);
+
+}

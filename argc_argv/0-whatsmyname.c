17449857b5168ed33aa3823388a382cded0cf5f7
fadumaaah <10507@holbertonstudents.com>
2025-03-20 03:59:21 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ *
+ * @argc: number of command line arguments
+ * @argv: array containing command line arguments
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
